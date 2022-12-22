b0942c1f1f429ca54e6a3344f96d876870036fc2
codewithashim <codewithashim@gmail.com>
2022-12-22 06:50:00 -0500

    adde some change

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hey there! wellcome to car docter server !!!");
+  res.send("Hey there! wellcome to car docter server starting car sever !!!");
 });
 
 /// ========================= DB Connection =========================
@@ -274,5 +274,5 @@ app.patch("/updateOrder/:id", async (req, res) => {
 ///========================= Midils Wares =========================
 
 app.listen(port, () => {
-  console.log(`Car Docter Server Is Runing  ${port}`.blue.bold);
+  console.log(`Car Docter Server Is Runing On Port ${port}`.blue.bold);
 });
